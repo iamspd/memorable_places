174ed0e99e5abafd9def88bb1bbd08129c654570
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-12-19 15:42:01 -0500

    MainActivity - intent implemented

--- app/src/main/java/com/example/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/memorableplaces/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // widgets
+    private ListView placesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        placesList = findViewById(R.id.listPlaces);
+    }
+
+    public void onAddNewPlacesClick(View view) {
+        startActivity(new Intent(this, MapsActivity.class));
     }
 }
